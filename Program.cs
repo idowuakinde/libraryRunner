9f53eb7232d331aff45b24faf27642d57f7effea
Idowu Akinde <8018104+idowuakinde@users.noreply.github.com>
2018-12-08 17:40:53 +0100

    Example ASP.NET Core 2.1 Console 'Library Runner' App to consume a console-built Class Library, based on Udemy tutorial...

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using stringModifierClassLibrary;
+
+namespace libraryRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ChangeMyString.MakeAllUpperCase("Hello World!"));
+            Console.WriteLine(ChangeMyString.MakeReverse("Hello World!"));
+        }
+    }
+}
